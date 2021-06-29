58caf0d3144f7876def2cffa6e583cebd3f644b0
GokalpSen <gokalpsen09@gmail.com>
2021-06-29 15:07:55 +0300

    r

--- F Clicker/Form1.Designer.cs
+++ F Clicker/Form1.Designer.cs
@@ -187,6 +187,7 @@ namespace F_Clicker
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
