5f38a44dbb134b010f917dfffd074483aa76b4dd
GokalpSen <gokalpsen09@gmail.com>
2021-06-29 15:53:59 +0300

    r

--- F Clicker/Form1.cs
+++ F Clicker/Form1.cs
@@ -13,7 +13,7 @@ namespace F_Clicker
 {
     public partial class Form1 : Form
     {
-        static string ver = "1.0.0beta";
+        static string ver = "1.0.0";
         string[] master = new string[13];
         double Fs = 0;
         double Fpc = 1;
